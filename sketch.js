376cf5025bdac3056b12b6bba916a7b41f79fe30
Zakiya435 <63589526+Zakiya435@users.noreply.github.com>
2020-05-05 19:07:20 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -100,7 +100,7 @@ async function getBackgroundImage()
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     //console.log(hour);
-    if((hour>=06)&&(hour<=19))
+    if((hour>=06)&&(hour<=15))
     {
         bg = "bg1.png";
     }
